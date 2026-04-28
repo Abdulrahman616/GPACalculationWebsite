d3feddb92296c27a304e44486674afadf28e9b95
Abdulrahman616 <akhalifi0004@stu.kau.edu.sa>
2026-04-28 21:45:49 +0300

    modifying and moving some functions to the script.js file

---
+++ script.js
@@ -0,0 +1,47 @@
+    function gradeToPoints(grade, scale) {
+        if (scale === 5) {
+            if (grade >= 95) return 5.0;
+            else if (grade >= 90) return 4.75;
+            else if (grade >= 85) return 4.5;
+            else if (grade >= 80) return 4.0;
+            else if (grade >= 75) return 3.5;
+            else if (grade >= 70) return 3.0;
+            else if (grade >= 65) return 2.5;
+            else if (grade >= 60) return 2.0;
+        else return 0.0;
+  }     else {
+    // if the GPA was up to 4
+        if (grade >= 90) return 4.0;
+        else if (grade >= 85) return 3.7;
+        else if (grade >= 80) return 3.3;
+        else if (grade >= 75) return 3.0;
+        else if (grade >= 70) return 2.7;
+        else if (grade >= 65) return 2.3;
+        else if (grade >= 60) return 2.0;
+        else return 0.0;
+  }
+}
+
+    
+    function calculateGPA() {
+        var scaleSelect = document.getElementById("gpaScale");      
+        var scale = parseInt(scaleSelect.value);       // conventing the selected scale to a intger 
+        var table = document.getElementById("calculator-table");
+        var totalHours = 0;
+        var totalPoints = 0;
+
+        for (var i = 1; i < table.rows.length; i++) {
+            var hours = parseFloat(table.rows[i].cells[1].querySelector("input").value);
+            var grade = parseFloat(table.rows[i].cells[2].querySelector("input").value);
+            totalHours = totalHours + hours;
+            totalPoints = totalPoints + (hours * gradeToPoints(grade, scale));   //using the selected scale to calculate the GPA
+        }
+
+
+        var gpa = totalPoints / totalHours;
+
+
+        document.getElementById("total-hours").textContent = totalHours;
+        document.getElementById("gpa-value").textContent = gpa.toFixed(2); // https://www.w3schools.com/jsref/prop_node_textcontent.asp was the guidance here
+        document.getElementById("gpa-result").style.display = "block";
+    }
